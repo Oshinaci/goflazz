735caff78cb5d5d5a531fabac8e7baf4309c5635
奥希纳慈 <oshinaci@yahoo.com>
2026-07-20 06:21:25 +0700

    Wrap content in main tag with styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
-    <>
+    <main className="bg-[#050816] text-white overflow-x-hidden">
       <Navbar />
       <Hero />
-    </>
+    </main>
   );
 }
